4d590661b43019487730ef18d8e6987084f9b2b6
goprodeptrai01 <tapnguyen01@gmail.com>
2023-07-01 18:40:36 +0700

    update api

--- src/routes/qrCodeRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import QRCodeController from "../controllers/QRCodeController";
-
-const router = express.Router();
-
-router.post("/generate/:productId", QRCodeController.generateQRCode);
-
-export default router;
